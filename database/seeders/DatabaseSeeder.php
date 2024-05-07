eeb342d69225cd2325db1c82912a63eb8b4166b2
Amsiam <amsiam990@gmail.com>
2024-05-08 00:39:52 +0600

    database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Zone;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -20,14 +21,26 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin@admin.com',
         ]);
 
+
+
+        $zone = Zone::create([
+            "name" => "Fec",
+            "address" => "Faridpur Engineering College"
+        ]);
+
+
         User::factory()->create([
             'name' => 'Collector',
             'email' => 'collector@admin.com',
+            "zone" => "Fec"
         ]);
 
         User::factory()->create([
             'name' => 'User',
             'email' => 'user@admin.com',
+            "zone" => "Fec",
+            "adress" => "South hall"
         ]);
+
     }
 }
